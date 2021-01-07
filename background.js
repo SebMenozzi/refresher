c89bfaff768288d8a45c0be200b8f167f899838f
Julien Roncaglia <julien.roncaglia@zen.ly>
2021-01-07 14:02:59 +0100

    Refresh all tabs and use the click for ON/OFF

--- background.js
+++ background.js
@@ -1,7 +1,58 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    setInterval(function() {
-        chrome.tabs.executeScript(tab.id, {
-            code: "location.reload()"
-        })
-    }, 60000)
-})
+const reloadDelay = 15 * 60 * 1000;
+
+function reloadAllTabs() {
+    console.log('Starting tab refresh');
+    chrome.tabs.query({}, tabs => {
+        for(const tab of tabs) {
+            console.log('Refreshing tab', tab.url);
+            chrome.tabs.reload(tab.id, { bypassCache: true });
+        }
+    });
+    console.log('End of tab refresh');
+}
+
+let intervalId = undefined;
+
+function isEnabled() {
+    return intervalId !== undefined;
+}
+
+function showStatus() {
+    const enabled = isEnabled();
+    const color = enabled ? 'green': 'red';
+    const text = enabled ? 'ON' : 'OFF';
+
+    chrome.browserAction.setBadgeBackgroundColor({ color });
+    chrome.browserAction.setBadgeText({ text });
+}
+
+function enable() {
+    if (isEnabled()) {
+        return;
+    }
+
+    intervalId = setInterval(reloadAllTabs, reloadDelay);
+    showStatus();
+    console.log('Reload enabled');
+}
+
+function disable() {
+    if (!isEnabled()) {
+        return;
+    }
+
+    clearInterval(intervalId);
+    intervalId = undefined;
+    showStatus();
+    console.log('Reload disabled');
+}
+
+chrome.browserAction.onClicked.addListener(() => {
+    if (isEnabled()) {
+        disable();
+    } else {
+        enable();
+    }
+});
+
+enable();
